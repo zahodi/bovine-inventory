642dbf50988579288d6abe5c3d25e3b4a9952d05
Valentin Gurmeza <valentin.gurmeza@emcconnected.com>
2016-09-05 22:29:45 -0700

    add os.walk add more sample folders for testing

--- bin/bovine.py
+++ bin/bovine.py
@@ -1,2 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import json
+import os
+##########################
+# Read the Static Directory
+##########################
+
+directory_files = os.walk('../static/')
+print(directory_files)
