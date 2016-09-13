45d916f8f744d73d5d545029362ecd615997e2b2
Valentin Gurmeza <valentin.gurmeza@emcconnected.com>
2016-09-12 23:46:53 -0700

    walk the the directory and load the yaml files

--- bin/bovine.py
+++ bin/bovine.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
-import json
 import os
+import yaml
 ##########################
 # Read the Static Directory
 ##########################
+def list_of_files(root_directory):
+  directory_files = os.walk(root_directory)
+  filenames = []
+  for root, dirs, files in directory_files:
+    for name in files:
+      filenames.append(os.path.join(root, name))
+  return filenames
+  # print(filenames)
 
-directory_files = os.walk('../static/')
-print(directory_files)
+
+def import_yaml(filenames):
+  for name in filenames:
+    with open(name, 'r') as f:
+      print(yaml.load(f))
+
+import_yaml(list_of_files('../test/test_data'))
