fba886dad1f8c0b17b492243d5659c1da014e4ba
Valentin Gurmeza <valentin.gurmeza@emcconnected.com>
2016-11-11 09:37:49 -0800

    testing

--- bin/bovine.py
+++ bin/bovine.py
@@ -4,19 +4,31 @@ import yaml
 ##########################
 # Read the Static Directory
 ##########################
-def list_of_files(root_directory):
-  directory_files = os.walk(root_directory)
-  filenames = []
-  for root, dirs, files in directory_files:
-    for name in files:
-      filenames.append(os.path.join(root, name))
-  return filenames
-  # print(filenames)
 
 
-def import_yaml(filenames):
-  for name in filenames:
-    with open(name, 'r') as f:
-      print(yaml.load(f))
+class Inventory:
+  def __init__(self, root_directory):
+    # get the files in the root_directory
+    # self.
+    self.root_directory = root_directory
+    # import the yaml from the files that we found above
+  def list_files(self, root_directory):
+    directory_files = os.walk(root_directory)
+    filenames = []
+    for root, dirs, files in directory_files:
+      for name in files:
+        filenames.append(os.path.join(root, name))
+    return filenames
+    # print(filenames)
 
-import_yaml(list_of_files('../test/test_data'))
+
+  def import_yaml(self, filenames):
+    load_of_yaml = []
+    for name in filenames:
+      with open(name, 'r') as f:
+        load_of_yaml = yaml.load(f)
+    return load_of_yaml
+
+test_inventory = Inventory('../test/test_data')
+list_of_files = test_inventory.list_files('../test/test_data')
+print(yaml.dump(list_of_files))
