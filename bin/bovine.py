e8897022d7a7a33419da9f4bd474a850ca080e4a
Valentin Gurmeza <valentin.gurmeza@emcconnected.com>
2016-11-11 14:16:13 -0800

    testing

--- bin/bovine.py
+++ bin/bovine.py
@@ -11,8 +11,9 @@ class Inventory:
     # get the files in the root_directory
     # self.
     self.root_directory = root_directory
+    self.filenames = filenames
     # import the yaml from the files that we found above
-  def list_files(self, root_directory):
+  def get_file_names(self, root_directory):
     directory_files = os.walk(root_directory)
     filenames = []
     for root, dirs, files in directory_files:
@@ -22,13 +23,16 @@ class Inventory:
     # print(filenames)
 
 
-  def import_yaml(self, filenames):
+  def import_yaml(self):
     load_of_yaml = []
     for name in filenames:
       with open(name, 'r') as f:
         load_of_yaml = yaml.load(f)
     return load_of_yaml
 
+  def get_inventory(self):
+    inventory = import_yaml()
+    return inventory
+
 test_inventory = Inventory('../test/test_data')
-list_of_files = test_inventory.list_files('../test/test_data')
-print(yaml.dump(list_of_files))
+print(yaml.dump(test_inventory.get_inventory()))
