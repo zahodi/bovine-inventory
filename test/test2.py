2336f37abd47ebdac00a6cbe247aa10c59a9811a
Shaun Smiley <senorsmile@gmail.com>
2017-01-26 22:53:49 -0800

    note about todo: namespacing

--- test/test2.py
+++ test/test2.py
@@ -3,6 +3,7 @@
 import sys
 sys.path.append('./lib/')
 
-from bovine.staticinventory import *
+### TODO: we seriously need to work on namespacing
+from bovine.staticinventory import * 
 
 i = StaticInventory(root_directory='./test/test_data/static/')
