59d929ec2d403a08b3a601575ba380bb8e0f4a7a
Shaun Smiley <senorsmile@gmail.com>
2017-05-24 14:22:43 -0700

    remove unused tests

--- test/test.py
+++ test/test.py
@@ -10,27 +10,6 @@ sys.path.append('lib/')
 from bovine.inventory import *
 
 
-class TestStringMethods(unittest.TestCase):
-
-    #################
-    # sanity tests
-    #################
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
-
 class TestStaticInventory(unittest.TestCase):
     ####################################
     # Test bovine.StaticInventory module
