8e3c2ee446b29049dcd5888364cb50c61ddf54a8
Valentin Gurmeza <valentin.gurmeza@globaleagle.com>
2017-08-23 14:24:54 -0700

    make test output easier to read

--- test/test_api.py
+++ test/test_api.py
@@ -19,36 +19,44 @@ class TestIntegrations(TestCase):
   def test_list_groups_vars(self):
     response = self.app.post('api/vars?action=list&name=group1&type=groups')
     payload = json.loads(response.get_data(response))
+    print("#########################################################")
     print("group1 vars are: ", payload)
     self.assertEqual(payload, {'foo': 'bar'})
 
   def test_list_hosts_vars(self):
     response = self.app.post('api/vars?action=list&name=host1&type=hosts')
     payload = json.loads(response.get_data(response))
+    print("#########################################################")
     print("host1 vars are: ", payload)
     self.assertEqual(payload, {'foo': 'bar1'})
 
   def test_list_children(self):
     response = self.app.post('api/children?action=list&name=group1')
     payload = json.loads(response.get_data(response))
+    print("#########################################################")
     print("group1 children are: ", payload)
     self.assertEqual(payload, ["group2", "group3", "group4"])
 
   def test_search_hosts(self):
     response = self.app.post('api/hosts?action=search&name=web')
     payload = json.loads(response.get_data(response))
+    print("#########################################################")
+    print("Searching for any hosts which includes keyword: web")
     print("Host search results are : ", payload)
     self.assertEqual(payload, ["web_host1", "web_host2"])
 
   def test_search_groups(self):
     response = self.app.post('api/groups?action=search&name=web')
     payload = json.loads(response.get_data(response))
+    print("#########################################################")
+    print("Searching for any groups which includes keyword: web")
     print("Group search results are : ", payload)
     self.assertEqual(payload, ["web_group1", "web_group2"])
 
   def test_search_hosts_no_response(self):
     response = self.app.post('api/hosts?action=search&name=group')
     payload = json.loads(response.get_data(response))
+    print("#########################################################")
     print("The response is : ", payload)
     self.assertEqual(payload, "no results")
 
